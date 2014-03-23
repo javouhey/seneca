2831a775616c3f8fcb927e8f1d6387841b220f00
Gavin Bong <gb.javouhey@gmail.com>
2014-03-23 13:43:15 -0700

    ran go fmt

--- util/args_test.go
+++ util/args_test.go
@@ -7,25 +7,25 @@ You may obtain a copy of the License at
 
      http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, 
-software distributed under the License is distributed on an 
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
-either express or implied. See the License for the specific 
-language governing permissions and limitations under the 
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific
+language governing permissions and limitations under the
 License.
 */
 
 package util_test
 
 import (
-    "testing"
     . "github.com/javouhey/seneca/util"
     "github.com/javouhey/seneca/vendor/github.com/stretchr/testify/assert"
+    "testing"
 )
 
 func TestEmptyCheck2(t *testing.T) {
-	
-	_, err := WidthOnly.Decode()
+
+    _, err := WidthOnly.Decode()
     if assert.Error(t, err, "An error was expected") {
         assert.Equal(t, err, ErrNoArgs)
     }
@@ -39,56 +39,56 @@ func TestEmptyCheck2(t *testing.T) {
     if assert.Error(t, err, "An error was expected") {
         assert.Equal(t, err, ErrScaleOutOfRange)
     }
- 
+
     _, err = ScaleType(5).Decode(500, 300, 150)
     if assert.Error(t, err, "An error was expected") {
         assert.Equal(t, err, ErrScaleOutOfRange)
     }
 
-	a, _ := WidthOnly.Decode(100)
+    a, _ := WidthOnly.Decode(100)
     assert.Equal(t, a, "scale=100:trunc(ow/a/2)*2")
 
-	_, err = WidthOnly.Decode(101)
+    _, err = WidthOnly.Decode(101)
     if assert.Error(t, err, "An error was expected") {
         assert.Equal(t, err.Error(), "101 must be even")
     }
 
-	_, err = HeightOnly.Decode()
+    _, err = HeightOnly.Decode()
     if assert.Error(t, err, "An error was expected") {
         assert.Equal(t, err, ErrNoArgs)
     }
 
-	a, _ = HeightOnly.Decode(666)
+    a, _ = HeightOnly.Decode(666)
     assert.Equal(t, a, "scale=trunc(oh*a/2)*2:666")
 
-	_, err = HeightOnly.Decode(661)
+    _, err = HeightOnly.Decode(661)
     if assert.Error(t, err, "An error was expected") {
         assert.Equal(t, err.Error(), "661 must be even")
     }
 
-	a, err = WidthHeight.Decode()
+    a, err = WidthHeight.Decode()
     if assert.Error(t, err, "An error was expected") {
         assert.Equal(t, err, ErrNoArgs)
     }
 
-	a, err = WidthHeight.Decode(640)
+    a, err = WidthHeight.Decode(640)
     if assert.Error(t, err, "An error was expected") {
         assert.Equal(t, err, ErrTwoArgs)
     }
 
-	a, _ = WidthHeight.Decode(640, 480)
+    a, _ = WidthHeight.Decode(640, 480)
     assert.Equal(t, a, "scale=640:480")
 
-	_, err = WidthHeight.Decode(641, 480)
+    _, err = WidthHeight.Decode(641, 480)
     if assert.Error(t, err, "An error was expected") {
         assert.Equal(t, err.Error(), "641 must be even")
     }
 
-	_, err = WidthHeight.Decode(640, 481)
+    _, err = WidthHeight.Decode(640, 481)
     if assert.Error(t, err, "An error was expected") {
         assert.Equal(t, err.Error(), "481 must be even")
     }
 
-	a, _ = WidthHeight.Decode(1280, 760, 481, 211)
+    a, _ = WidthHeight.Decode(1280, 760, 481, 211)
     assert.Equal(t, a, "scale=1280:760")
 }
